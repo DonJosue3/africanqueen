d2ec2ab5c8ea768003e03bf39b14d5a572b7fb9d
ISHAK <ishakaryayodonjosue@gmail.com>
2026-07-23 21:32:12 +0200

    'first'

--- website/urls.py
+++ website/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path('home/',views.home,name="home"),
     path("services/", views.services, name="services"),
     path("menu/", views.menu, name="menu"),
     path("shop/", views.shop, name="shop")
